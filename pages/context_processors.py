6e4df64686371479998269c3bf7c63c69cb79750
bthinkxdev <bthinkx@gmail.com>
2026-06-26 01:23:34 +0530

    og updated

--- pages/context_processors.py
+++ pages/context_processors.py
@@ -1,11 +1,16 @@
+from django.templatetags.static import static
+
 from .contact_info import contact_context
 
 
 def site_contact(request):
     info = contact_context()
+    og_image_url = request.build_absolute_uri(static("img/heroimage.png"))
     return {
         "contact": info,
         "company_name": info["company_name"],
         "contact_name": info["contact_name"],
         "contact_role": info["contact_role"],
+        "og_image_url": og_image_url,
+        "canonical_url": request.build_absolute_uri(),
     }
